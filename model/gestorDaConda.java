ab6320349487dc2462a0d555094ffec374e8473c
LaSedia <leonardoffline@gmail.com>
2017-08-18 21:17:20 -0300

    leo top

--- model/gestorDaConda.java
+++ model/gestorDaConda.java
@@ -1,4 +1,13 @@
 
-public class gestorDaConda {
+public class gestorDaConda extends funcionario {
+    private int idGestorDaConta;
+
+	public int getIdGestorDaConta() {
+		return idGestorDaConta;
+	}
+
+	public void setIdGestorDaConta(int idGestorDaConta) {
+		this.idGestorDaConta = idGestorDaConta;
+	}	
 
 }
--- model/venda.java
+++ model/venda.java
@@ -1,4 +1,27 @@
+import java.util.GregorianCalendar;
 
 public class venda {
+	private int idVenda;
+	private GregorianCalendar dataVenda = new GregorianCalendar();
+    private int status;
+    
+	public int getIdVenda() {
+		return idVenda;
+	}
+	public void setIdVenda(int idVenda) {
+		this.idVenda = idVenda;
+	}
+	public GregorianCalendar getDataVenda() {
+		return dataVenda;
+	}
+	public void setDataVenda(GregorianCalendar dataVenda) {
+		this.dataVenda = dataVenda;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
 
 }
